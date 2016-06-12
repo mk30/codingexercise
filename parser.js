a0b4edb0594a4606739f8b8812c4ec2aa1156e32
Marina Kukso <marina.kukso@gmail.com>
2016-06-11 22:14:22 -0500

    getting numbers in the correct order, in sets of 9

--- parser.js
+++ parser.js
@@ -3,47 +3,29 @@ var fs = require('fs')
 var fulltext = fs.readFileSync(process.argv[2]).toString()
 var maxn = (fulltext.split('\n').length - 1)/4
 
-for (var n = 0; n < maxn; n++){
-  for (var i = 112*n; i < 27+(112*n); i+=3) {
-    console.log(fulltext.substr(i, 3))
-    console.log(fulltext.substr(i + 28, 3))
-    console.log(fulltext.substr(i + (28*2), 3))
-    console.log(fulltext.substr(i + (28*3), 3))
-  } 
+var rosettaStone = {
+  ' _ | ||_|   ': 0,
+ '     |  |   ':1, 
+  ' _  _||_    ' :2,
+ ' _  _| _|   ' :3,
+  '   |_|  |   ' :4,
+ ' _ |_  _|   ':5,
+ ' _ |_ |_|   ' :6,
+ ' _   |  |   ' :7,
+  ' _ |_||_|   ' :8,
+  ' _ |_| _|   ' :9
 }
 
-var zero = [ ' _ ', '| |', '|_|', '   ' ] 
-var one = [ '   ', '  |', '  |', '   ' ]
-var two = [ ' _ ', ' _|', '|_ ', '   ' ]
-var three = [ ' _ ', ' _|', ' _|', '   ' ]
-var four = [ '   ', '|_|', '  |', '   ' ]
-var five = [ ' _ ', '|_ ', ' _|', '   ' ]
-var six = [ ' _ ', '|_ ', '|_|', '   ' ]
-var seven = [ ' _ ', '  |', '  |', '   ' ]
-var eight = [ ' _ ', '|_|', '|_|', '   ' ]
-var nine = [ ' _ ', '|_|', ' _|', '   ' ]
-/*
-for (var i = 0; i < nums.length; i++){
-  if (JSON.stringify(nums[i]) == JSON.stringify(zero))
-    console.log(0)
-  else if (JSON.stringify(nums[i]) == JSON.stringify(one))
-    console.log(1)
-  else if (JSON.stringify(nums[i]) == JSON.stringify(two))
-    console.log(2)
-  else if (JSON.stringify(nums[i]) == JSON.stringify(three))
-    console.log(3)
-  else if (JSON.stringify(nums[i]) == JSON.stringify(four))
-    console.log(4)
-  else if (JSON.stringify(nums[i]) == JSON.stringify(five))
-    console.log(5)
-  else if (JSON.stringify(nums[i]) == JSON.stringify(six))
-    console.log(6)
-  else if (JSON.stringify(nums[i]) == JSON.stringify(seven))
-    console.log(7)
-  else if (JSON.stringify(nums[i]) == JSON.stringify(eight))
-    console.log(8)
-  else if (JSON.stringify(nums[i]) == JSON.stringify(nine))
-    console.log(9)
-  else return
+for (var n = 0; n < maxn; n++){
+  for (var i = 112*n; i < 27+(112*n); i+=3) {
+    console.log(
+      rosettaStone[
+        fulltext.substr(i, 3) + 
+        fulltext.substr(i + 28, 3) + 
+        fulltext.substr(i + (28*2), 3) + 
+        fulltext.substr(i + (28*3), 3)
+      ]
+    )
+  } 
+  console.log('\n')
 }
-*/
